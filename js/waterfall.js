97a143a711c2e520863468547a0f0e15f441169a
LuJun <lujun723@gmail.com>
2017-03-16 16:19:22 +0800

    demo1 updata

--- js/waterfall.js
+++ js/waterfall.js
@@ -14,12 +14,12 @@
      */
     var demoContent = [
         {
-            demo_link: 'https://codepen.io/haoyang/pen/jrvrQq',
-            img_link: 'https://ooo.0o0.ooo/2016/11/24/5836d81f48cd2.png',
-            code_link: 'https://codepen.io/haoyang/pen/jrvrQq',
-            title: 'Fisher-Yates 洗牌算法动画',
-            core_tech: 'JavaScript',
-            description: 'Fisher-Yates 洗牌算法动画。算法详情见 <a href ="https://gaohaoyang.github.io/2016/10/16/shuffle-algorithm/">这里</a>。'
+            demo_link: 'https://create.arduino.cc/projecthub/otto/otto-build-your-own-robot-in-two-hours-5f2a1c',
+            img_link: 'https://hackster.imgix.net/uploads/image/file/152711/FL32WPLIO8U35NB.LARGE.jpg?w=680&h=510&fit=max&fm=jpg',
+            code_link: 'https://create.arduino.cc/projecthub/otto/otto-build-your-own-robot-in-two-hours-5f2a1c',
+            title: 'Otto 做你自己的小机器人',
+            core_tech: 'Arduino',
+            description: 'Otto小机器人制作教程，详见 <a href ="https://create.arduino.cc/projecthub/otto/otto-build-your-own-robot-in-two-hours-5f2a1c">这里</a>。'
         },{
         demo_link: 'http://gaohaoyang.github.io/test/headerTransition/',
         img_link: 'https://ooo.0o0.ooo/2016/06/20/5768c1597d1fe.png',
